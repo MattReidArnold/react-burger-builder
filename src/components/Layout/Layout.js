cdf5accf167dd3e52038a893d2b199ab7721edbd
Matt Arnold <marnold@gospotcheck.com>
2018-07-03 17:47:30 -0600

    add toolbar with logo

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,14 +1,13 @@
-import React, { Fragment } from 'react';
+import React, { Fragment } from "react";
 
-import classes from './Layout.css';
+import classes from "./Layout.css";
+import Toolbar from "../Navigation/Toolbar/Toolbar";
 
-const layout = (props) => (
-    <Fragment>
-        <div>Toolbar, SideDrawer, Backdrop</div>
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Fragment>
+const layout = props => (
+  <Fragment>
+    <Toolbar />
+    <main className={classes.Content}>{props.children}</main>
+  </Fragment>
 );
 
 export default layout;
